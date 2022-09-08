86e5aaa2aa150274179a1098c70a76c0b0c31b14
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-09-08 10:53:03 +0530

    DSA BASICS

---
+++ src/DataType/TypeCasting.java
@@ -0,0 +1,9 @@
+package DataType;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        byte a = (byte) 330;
+        byte b = (byte) 428;
+        System.out.println(a + " " + b);
+    }
+}
